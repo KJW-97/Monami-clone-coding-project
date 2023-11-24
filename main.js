c3bb46617d4aa938a27c03a5141dd2512ada617a
KJW-97 <73945452+KJW-97@users.noreply.github.com>
2023-11-24 17:30:46 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// const badgeEl = document.querySelector('header .popup');
+
+// window.addEventListener('scroll', _.throttle(function () {
+//   console.log(window.scrollY);
+//   if(window.scrollY > 500){
+//     //배지 숨기기
+//     //gsap.to(요소, 지속시간, 옵션);
+//     gsap.to(badgeEl, .6, {
+//       opacity: 0,
+//       display: 'none'
+//     });
+//   }
+//   else {
+//     //배지 보이기
+//     gsap.to(badgeEl, .6 ,{
+//       opacity: 1,
+//       display: 'block'
+//     });
+//   }
+// }, 300));
